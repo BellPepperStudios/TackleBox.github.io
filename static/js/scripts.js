32160a4cc5eaccc03092745bb19c6e056eb38849
dill <131902507+BellPepperStudios@users.noreply.github.com>
2025-07-16 07:39:19 -0700

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,6 @@
 const inputelem = document.getElementById("username");
 const outputelem = document.getElementById("output");
 const errorelem = document.getElementById("error");
-const stringbean = "troutmate";
 const eu = "U2FsdGVkX1/CqPn+f/wUqIHEhnep8VOn0rSZRXk4u/M="; 
 const ep = "U2FsdGVkX1+f4bFBPTla9AZQG+tJCs1a4lcbDWrJZ3nNgQ/9Zf+7R3AX3r8FcBHb"; 
 const w = "U2FsdGVkX19OqXOyU0yKC1yrFU7TN0ExI2omFT9bOGGYzhgjvDx8kvHGeaN1kH/m";
@@ -18,6 +17,7 @@ inputelem.addEventListener("input", function() {
 function login() {
     const inputUser = document.getElementById("username").value;
     const inputPass = document.getElementById("password").value;
+    const stringbean = document.getElementById("key").value;
 
     const du = CryptoJS.AES.decrypt(eu, stringbean).toString(CryptoJS.enc.Utf8);
     const dp = CryptoJS.AES.decrypt(ep, stringbean).toString(CryptoJS.enc.Utf8);
